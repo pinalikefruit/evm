c7d311d2396e21468c53dbb56858d6fde03f0cd3
pinalikefruit <pinajoma0@gmail.com>
2023-05-26 10:14:41 -0400

    [ADD] execution environment

---
+++ src/classes/execution/errors.ts
@@ -0,0 +1,5 @@
+class InvalidBytecode extends Error {
+
+}
+
+export { InvalidBytecode};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hellow, world");
+import { argv } from "process";
+import ExecutionContext from "./classes/execution";
+
+
+const main = () => {
+    const code = argv[2] ?? "0x00";
+    const executionContext = new ExecutionContext(code)
+
+    executionContext.run();
+}
+
+main();
